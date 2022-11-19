2db99b79e8a74f7169ba2432b319fed2b75860b6
edwin9905 <qkr990509@naver.com>
2022-11-19 19:27:23 +0900

    1119/1927

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -5,39 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/18 23:08:28 by gkwon             #+#    #+#             */
-/*   Updated: 2022/11/18 23:18:41 by gkwon            ###   ########.fr       */
+/*   Created: 2022/11/08 21:11:09 by soulee            #+#    #+#             */
+/*   Updated: 2022/11/19 19:26:41 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	count_digits(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 0)
+		count++;
+	if (n == 0)
+		count++;
+	while (n)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*ret;
-	int		tmp;
-	int		cnt;
-	int		neg;
-	
-	neg = 0;
-	cnt = 0;
-	tmp = n;
+	int		len;
+	char	*result_str;
+
+	len = count_digits(n);
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	result_str = malloc(sizeof(char) * (len + 1));
+	if (!result_str)
+		return (0);
+	result_str[len--] = 0;
 	if (n < 0)
-		neg = 1;
-	while (tmp)
 	{
-		tmp /= 10;
-		cnt++;
-	}
-	ret = (char *)malloc(cnt + 1 + neg);
-	if (neg)
-	{
-		ret[0] = '-';
-		ret++;
+		result_str[0] = '-';
 		n *= -1;
 	}
 	while (n)
 	{
-		
+		result_str[len--] = ('0' + (n % 10));
+		n /= 10;
 	}
+	return (result_str);
 }
