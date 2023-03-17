5c1a1915649ca9f6283c59dc3d36350e13beff88
edwin9905 <qkr990509@naver.com>
2023-03-17 16:54:16 +0900

    0318

--- philo/main.c
+++ philo/main.c
@@ -1,39 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/19 20:10:30 by gkwon             #+#    #+#             */
-/*   Updated: 2023/03/16 20:18:28 by gkwon            ###   ########.fr       */
+/*   Created: 2023/03/16 21:21:22 by gkwon             #+#    #+#             */
+/*   Updated: 2023/03/16 21:22:06 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
-
-# include <signal.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include "ft_printf/ft_printf.h"
-
-# define INT_MAX 2147483647
-# define INT_MIN -2147483648
-
-typedef struct s_client_info
+int	main(int argc, char **argv)
 {
-	char	*str;
-	int		bits_cnt;
-	pid_t	server_pid;
-}			t_client_info;
 
-typedef struct s_server_info
-{
-	int		client_pid;
-	char	msg;
-	int		bits;
-	int		cnt;
-}			t_server_info;
-
-#endif
+	
+}
--- philo/philo.h
+++ philo/philo.h
@@ -1,39 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/19 20:10:30 by gkwon             #+#    #+#             */
-/*   Updated: 2023/03/16 20:18:28 by gkwon            ###   ########.fr       */
+/*   Created: 2023/03/16 21:21:31 by gkwon             #+#    #+#             */
+/*   Updated: 2023/03/16 21:21:32 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
-
-# include <signal.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include "ft_printf/ft_printf.h"
-
-# define INT_MAX 2147483647
-# define INT_MIN -2147483648
-
-typedef struct s_client_info
-{
-	char	*str;
-	int		bits_cnt;
-	pid_t	server_pid;
-}			t_client_info;
-
-typedef struct s_server_info
-{
-	int		client_pid;
-	char	msg;
-	int		bits;
-	int		cnt;
-}			t_server_info;
-
-#endif
