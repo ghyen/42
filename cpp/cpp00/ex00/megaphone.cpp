2f12fc28354c88962e005be8bebc0449175ff681
edwin9905 <qkr990509@naver.com>
2023-08-18 17:29:48 +0900

    cpp00_ex00

---
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv){
+	if(argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int j = 1; j < argc; j++)
+			for (size_t i = 0; i < strlen(argv[j]); i++)
+				putchar(toupper(argv[j][i]));
+		std::cout << std::endl;
+	}
+    return 0;
+}
