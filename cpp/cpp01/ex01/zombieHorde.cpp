cb4449c56d5d002a81c7c7fb0461ff796dc47045
edwin9905 <qkr990509@naver.com>
2023-10-19 16:32:27 +0900

    cpp01 ex01

---
+++ cpp/cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name)
+{
+    Zombie *ZombiesAddress;
+    while (N)
+    {
+        ZombiesAddress = newZombie(<cast>string:N);
+        ZombiesAddress += sizeof(Zombie);
+
+    return (ZombiesAddress);
+}
