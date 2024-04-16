bd945d7ea700dea253c86bc49e8cc4e9631e5948
edwin9905 <qkr990509@naver.com>
2024-04-16 21:38:37 +0900

    inception complete

--- inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 echo start setting nginx
 
-apt-get -y update &&
-apt-get -y upgrade &&
-apt-get -y install \
-nginx \
-openssl \
-vim
-
 openssl	req -x509 -nodes -days 365 -newkey rsa:2048 \
 		-keyout /etc/ssl/private/server_pkey.pem \
 		-out /etc/ssl/certs/server.crt \
 		-subj "/C=KR/ST=Seoul/L=Gaepo/O=42Seoul/OU=./CN=gkwon.42.fr/emailAddress=gkwon@student.42seoul.kr"
-yes | cp var/www/html/default /etc/nginx/sites-available/default
 
 echo finish setting nginx
 exec "$@"
--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,38 +1,25 @@
-#!/bin/bash
+sleep 10
 
-echo start setting wordpress
-apt-get -y update &&
-apt-get -y upgrade &&
-apt-get -y install \
-php7.3 \
-php-fpm \
-php-cli \
-wget \
-curl \
-php-mysql \
-php-mbstring \
-php-xml \
-sendmail \
-vim
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-service php7.3-fpm start;
-apt-get -y install mariadb-client
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+mkdir -p /run/php
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-  echo 1
-  curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-  echo 2
-  chmod +x wp-cli.phar
-  mv wp-cli.phar /usr/local/bin/wp
+cd /var/www/html
 
-  wp core download --allow-root --path=/var/www/html/
-  tail -f
-  #wp core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PWD --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root --path=/var/www/html/
-  wp core install --url=https://$DOMAIN_NAME --title="gkwon's inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL --allow-root --path=/var/www/html/
-  wp user create "$USER_NAME" "$USER_EMAIL" --role=subscriber --user_pass="$USER_PWD" --allow-root --path=/var/www/html/
-fi
+wp core install --allow-root --path=/var/www/html \
+								--url=$DOMAIN_NAME \
+								--title=Inception \
+								--admin_user=$ADMIN_NAME \
+								--admin_password=$ADMIN_PWD \
+								--admin_email=$ADMIN_EMAIL
 
-service php7.3-fpm stop;
-echo finish setting wordpress
-exec "$@"
+wp user create \
+							$USER_NAME $USER_EMAIL \
+							--allow-root --path=/var/www/html \
+							--role=author \
+							--user_pass=$USER_PWD
+
+sleep 5
+exec  php-fpm7.4 -F
