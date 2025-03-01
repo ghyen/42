e1e7f3c266bb86d2606aa632832d838e2e27943b
gkwon <gkwon@student.42seoul.kr>
2025-03-01 18:53:11 +0900

    python

---
+++ python/0_Starting/ex00/Hello.py
@@ -0,0 +1,16 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "Seoul!")
+ft_set.remove("tutu!")
+ft_set.add("Seoul!")
+ft_dict["Hello"] = "42Seoul!"
+
+print(ft_list)
+print(ft_tuple)
+print(sorted(ft_set))
+print(ft_dict)
+
